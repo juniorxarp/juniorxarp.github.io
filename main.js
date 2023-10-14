dd5ed04e016dd5d3f2bc27225d8f9d858e407488
juniorxarp <xicorrj@hotmail.com>
2023-10-14 02:32:16 -0300

    1.0.5

--- main.js
+++ main.js
@@ -32,13 +32,11 @@ var nyexp = document.querySelector('#btn-menu')
 var menuIcon = document.querySelector('#menu-mobile')
 
 nyexp.addEventListener('click', function(){
-    menuIcon.classList.toggle('expandir')
+    menuIcon.classList.add('expandir')
 })
 
-// let btnMenu = document.getElementById('btn-menu')
-// let menu = document.getElementById('menu-mobile')
+menuIcon.addEventListener('click', function(){
+    menuIcon.classList.remove('expandir')
+})
 
-// btnMenu.addEventListener.add('click', ()=>{
-//     menu.classList.add('abrir')
-// })
 
